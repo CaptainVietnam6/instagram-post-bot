c3863496bfa07945aa1a49ed80fb0716a0636302
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-04-06 01:00:30 -1000

    updated code for upload to github

--- main_post_bot.py
+++ main_post_bot.py
@@ -2,6 +2,6 @@ from instabot import Bot
 
 bot = Bot()
 
-bot.login(username = "itz_kietttttttttt", password = "")
+bot.login(username = "user_name", password = "password")
 
 bot.upload_photo("bot.jpg", caption = "this post was uploaded by a bot for demonstation and testing purposes, will be removed momentarily")
