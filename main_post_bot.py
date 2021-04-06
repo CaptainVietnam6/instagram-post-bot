83b020369f06f83ab01f06233778af0f0b288f45
CaptainVietnam6 <kiet.riley2005@gmail.com>
2021-04-06 00:59:43 -1000

    first commit

---
+++ main_post_bot.py
@@ -0,0 +1,7 @@
+from instabot import Bot
+
+bot = Bot()
+
+bot.login(username = "itz_kietttttttttt", password = "")
+
+bot.upload_photo("bot.jpg", caption = "this post was uploaded by a bot for demonstation and testing purposes, will be removed momentarily")
